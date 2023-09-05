672dffa3a4adc89b2078c815753760a6a272c50b
IlmanNashran <ilmannashran@gmail.com>
2023-09-05 23:47:37 +0800

    create top row

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -10,6 +10,47 @@ class HomePage extends StatefulWidget {
 class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        top: true,
+        left: false,
+        right: false,
+        bottom: true,
+        minimum: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.05,
+            0, MediaQuery.of(context).size.width * 0.05, 0),
+        child: _mainTopColumn(),
+      ),
+    );
+  }
+
+  Widget _mainTopColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.menu,
+              color: Colors.black87,
+            ),
+            Container(
+              height: 39,
+              width: 144,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/logo_discover.png'),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
